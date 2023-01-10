75109aceca8d231a30dd82c816abc0761861ebbe
Joe Monkila <jnkiwa25@gmail.com>
2023-01-10 13:10:45 +0100

    commit

--- src/app/dashboard/pages/gs-student/gs-student.component.ts
+++ src/app/dashboard/pages/gs-student/gs-student.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CrudService } from 'src/app/auth/crud.service';
+import { Student } from 'src/app/model/student';
 
 @Component({
   selector: 'app-gs-student',
@@ -7,9 +11,60 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GsStudentComponent implements OnInit {
 
-  constructor() { }
+  Student!:Student[];
+  studentForm:FormGroup;
+
+  constructor(
+    public crud: CrudService, 
+    public formBuilder: FormBuilder, 
+    public router: Router,
+    public act: ActivatedRoute,
+    ) {
+    this.studentForm = this.formBuilder.group({
+      nom: [''],
+      postnom: [''],
+      prenom: [''],
+      adresse: [''],
+      telephone: [''],
+      statusF: [''],
+      
+    })
+
+    
+   }
+
+
 
   ngOnInit(): void {
+
+    this.crud.getAll().subscribe(res => {
+      
+      this.Student = res.map( e => {
+        return{
+          id: e.payload.doc.id,
+          ...e.payload.doc.data() as {}
+        }as Student;
+      })
+     }) 
+
   }
+
+  deleteStudent(Student: { id: string | undefined; }){
+    if(confirm("Voulez vous supprimer cette information")){
+      this.crud.deleteStudent(Student);
+    }
+  }
+
+  
+  
+  onSubmit(){
+   
+    if(confirm("Voulez vous enregistrer")){
+
+      this.crud.addStudent(this.studentForm.value);
+      this.router.navigate(['/gs-student']);
+    }
+    
+  };
 
 }
--- src/app/model/student.ts
+++ src/app/model/student.ts
@@ -1,2 +1,11 @@
 export class Student {
+    id:string|undefined;
+    nom:string|undefined;
+    postnom:string|undefined;
+    prenom:string|undefined;
+    adresse:string|undefined;
+    telephone:string|undefined;
+    statusF:string|undefined;
+    empreinte:string|undefined;
+    
 }
