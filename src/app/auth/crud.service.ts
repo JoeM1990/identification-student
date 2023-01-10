175b8f6a6e2e24843977801b4b7447e37634a321
Joe Monkila <jnkiwa25@gmail.com>
2023-01-10 12:13:20 +0100

    commit

--- src/app/auth/crud.service.ts
+++ src/app/auth/crud.service.ts
@@ -1,9 +1,43 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { Student } from '../model/student';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CrudService {
 
-  constructor() { }
+  constructor(private angularFirestore: AngularFirestore) {}
+  
+   
+    getAll(){
+      return this.angularFirestore.collection('gs-student')
+      .snapshotChanges();
+    }
+
+    
+   
+
+    addStudent(student:Student){
+      return new Promise<any>((resolve, reject) => {
+        this.angularFirestore.collection('gs-student')
+        .add(student)
+        .then(response => { }, error => reject(error))
+        ;
+        })
+      } 
+
+    
+    deleteStudent(student: { id: string | undefined; }){
+      return this.angularFirestore.collection('gs-student')
+      .doc(student.id)
+      .delete();
+    }
+
+    getStatus(){
+      return this.angularFirestore.collection('gs-student')
+      .snapshotChanges();
+    }
+
+    
 }
