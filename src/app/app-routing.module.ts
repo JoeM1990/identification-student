7e24ffe9d41538bdac6e18f1abca4df86af452c8
Joe Monkila <jnkiwa25@gmail.com>
2023-01-27 15:08:11 +0100

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './auth/auth.guard';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { GsStudentComponent } from './dashboard/pages/gs-student/gs-student.component';
 import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'login' },
-  { path: 'dashboard', component:DashboardComponent},
+  { path: 'dashboard', component:DashboardComponent,canActivate:[AuthGuard]},
   { path: 'login', component:LoginComponent},
-  { path: 'gs-student', component:GsStudentComponent},
+  { path: 'gs-student', component:GsStudentComponent,canActivate:[AuthGuard]},
 ];
 
 @NgModule({
