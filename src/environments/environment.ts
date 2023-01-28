f34e32d811ece6399fd60c778e30cf7689fc669e
JoeMonkila <70669743+JoeM1990@users.noreply.github.com>
2023-01-28 10:58:55 +0100

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,16 +4,7 @@
 
 export const environment = {
   production: true,
-  firebase : {
-    apiKey: "AIzaSyC3uNY6ruO5RnJrhFLuGF1u5CT-NQOo9VA",
-  authDomain: "gestion-college-joel.firebaseapp.com",
-  databaseURL: "https://gestion-college-joel-default-rtdb.firebaseio.com",
-  projectId: "gestion-college-joel",
-  storageBucket: "gestion-college-joel.appspot.com",
-  messagingSenderId: "649159510132",
-  appId: "1:649159510132:web:662777f28891ee723be921",
-  measurementId: "G-7402W56PQ9"
-  }
+  
 };
 
 /*
